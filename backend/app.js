8eddcbf67e1d0d0cf2aa5579d8bab91088597960
Ansh Modi <ansh@pruthatek.com>
2024-02-21 22:48:23 +0530

    changes

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const connection = require('./Connections/mongoConnection.js');
-const routes = require('./Routes/routes.js');
+const express = require("express");
+const mongoose = require("mongoose");
+const connection = require("./Connections/mongoConnection.js");
+const routes = require("./Routes/routes.js");
 const cors = require("cors");
-const cookieparser = require('cookie-parser');
-const dotenv = require('dotenv');
-const path =require('path');
+const cookieparser = require("cookie-parser");
+const dotenv = require("dotenv");
+const path = require("path");
 dotenv.config({
-    path: './config.env'
-})
+  path: "./config.env",
+});
 
 const app = express();
 
@@ -16,21 +16,19 @@ app.use(express.json());
 
 app.use(cookieparser());
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.Base,
-}));
+  })
+);
 
 connection();
 
 app.use(routes);
 
-app.use(express.static(path.join(__dirname,'./payday2/build')));
-app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,"./payday2/build/index.html"))
+app.use(express.static(path.join(__dirname, "./payday2/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./payday2/build/index.html"));
 });
- 
 
 app.listen(process.env.PORT || 8000);
-
-
-
