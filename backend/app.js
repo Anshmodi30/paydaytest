705eafc3cab73139bafe39627cf9b724a6595ce8
Ansh Modi <ansh@pruthatek.com>
2024-02-22 15:18:54 +0530

    ...

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const connection = require("./Connections/mongoConnection.js");
-const routes = require("./Routes/routes.js");
+const express = require('express');
+const mongoose = require('mongoose');
+const connection = require('./Connections/mongoConnection.js');
+const routes = require('./Routes/routes.js');
 const cors = require("cors");
-const cookieparser = require("cookie-parser");
-const dotenv = require("dotenv");
-const path = require("path");
+const cookieparser = require('cookie-parser');
+const dotenv = require('dotenv');
+const path =require('path');
 dotenv.config({
-  path: "./config.env",
-});
+    path: './config.env'
+})
 
 const app = express();
 
@@ -16,19 +16,17 @@ app.use(express.json());
 
 app.use(cookieparser());
 
-app.use(
-  cors({
-    origin: process.env.Base,
-  })
-);
+app.use(cors({
+    origin: "https://payday1.vercel.app/",
+}));
 
 connection();
 
 app.use(routes);
 
-app.use(express.static(path.join(__dirname, "./payday2/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./payday2/build/index.html"));
-});
+ 
 
 app.listen(process.env.PORT || 8000);
+
+
+
